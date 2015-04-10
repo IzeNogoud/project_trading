7464753bcb4f2ab368a00a384ca29b4727eabadd
IzeNogoud <robin.callerame@gmail.com>
2015-04-10 18:40:35 +0200

    fenetre FAQ et a propos terminees

--- developpement/projet3_trading_RC/faq.cpp
+++ developpement/projet3_trading_RC/faq.cpp
@@ -8,13 +8,41 @@ Faq::Faq(QWidget *parent) : QDialog(parent)
     setWindowTitle("FAQ");
 
     QTabWidget *Tab = new QTabWidget(this);
-    Tab->setGeometry(30, 20, 800, 650);
+    Tab->setGeometry(30, 20, 770, 450);
 
     QLabel *frenchText = new QLabel;
     QLabel *englishText = new QLabel;
 
-    frenchText->setText("Comment utiliser Flutu'Action ?");
-    englishText->setText(tr("<b>How to use Flutu'Action ?</b><br />Blablabla"));
+    frenchText->setText("<b><i>Comment utiliser Flutu'Action ?</i></b><br /><br />"
+                        "Fluctu'Action est une application permettant de suivre en temps réel"
+                        " l'évolution des cotations, en recherchant dynamiquement<br /> les données sur"
+                        "http://www.forexpro.com.<br /><br /><br />"
+                        "<b><i>Pour qui est destiné Fluctu'Action ?</i></b><br /><br />"
+                        "Fluctu'Action a été crée pour tout utilisateur désirant suivre le cours des monnaies internationnales.<br /><br /><br />"
+                        "<b><i>Pourquoi Fluctu'Action ? </i></b><br /><br />"
+                        "Permettre un accès rapide et intuitif afin de ne plus perdre de temps.<br /><br /><br />"
+                        "<b><i>Comment utiliser Fluctu'Action ? </i></b><br /><br />"
+                        "Fluctu'Action est très simple d'utilisation. Pour pouvoir afficher le tableau de devise voulu,"
+                        " il suffit de cliquer sur le couple<br /> de devise dans la barre de menu afin d'afficher les"
+                        " dernières données téléchargées.Pour pouvoir changer la source<br />  de récupération de données,"
+                        " il vous faudra modifier l'adresse dans Aide->Configuration, indiquer l'adresse URL puis Valider. ");
+
+
+
+
+    englishText->setText("<b><i>What is Fluctu'Action ? </i></b><br /><br />"
+                         "Fluctu'Action is an application that follows up in real time"
+                         " the evolution of quotations by dynamically seeking data<br /> on "
+                         "http://www.forexpro.com.<br /><br /><br />"
+                         "<b><i>Who is it for ? </i></b><br /><br />"
+                         "It was created for any user wishing to follows up the internationnal market price.<br /><br /><br />"
+                         "<b><i>Why use it ? </i></b><br /><br />"
+                         "Allow quick and intuitive access saving time.<br /><br /><br />"
+                         "<b><i>How use it ? </i></b><br /><br />"
+                         "Fluctu'Action is very easy to use. To display the currency table,"
+                         " Just click on the currency pair in the menu bar to display<br /> the last data downloaded."
+                         "To change the source data recovery, you need to modify the address in Help-> Configuration,"
+                         " specify the<br /> URL and then validate.");
 
 
     Tab->addTab(frenchText, "Français");
--- developpement/projet3_trading_RC/help.cpp
+++ developpement/projet3_trading_RC/help.cpp
@@ -1,29 +1,48 @@
 #include "help.h"
 #include <QLabel>
+#include <QTabWidget>
 #include <QVBoxLayout>
 
 
 Help::Help(QWidget *parent = 0) : QDialog(parent)
 {
     setWindowTitle("A propos de Fluctu'Action"); //Titre de la fenetre
-    setFixedSize(500,400);
 
-    closeButton = new QPushButton("Fermer"); //Bouton pour fermer la fenetre d'aide
-    closeButton->setCursor(Qt::PointingHandCursor);
-    connect(closeButton, SIGNAL(clicked()) ,this, SLOT(accept())); //Slot de fermeture avec le bouton
+
+    QTabWidget *Tab = new QTabWidget(this);
+    Tab->setGeometry(15, 20, 470, 350);
+
+    QLabel *frenchText = new QLabel;
+    QLabel *englishText = new QLabel;
 
     //Insertion du texte
-    QLabel *texte = new QLabel(tr("<b>A propos de Fluctu'Action</b><br />"
-                                  "bla bla bla<br /"
-                                  "><br /><b>About Fluctu'Action</b><br />"
-                                  "bla bla bla")); //Insertion du texte
+    frenchText->setText("<b>A propos</b><br /><br />"
+                                  "<i>Nom</i> : Fluctu'Action<br /><br />"
+                                  "<i>Version</i>  :  1.0<br /><br />"
+                                  "<i>Basé sur</i> : Qt 5.4.1<br /><br />"
+                                  "Compilé le <br /><br />"
+                                  "<i>Copyright 2015 Tous droits réservés</i><br /><br />"
+                                  "Ce program est sous licence GNU GPL version 3<br /><br />"
+                                  "Le programme est fourni TEL QUEL sans GARANTIE D'AUCUNE SORTE,<br />"
+                                  " Y COMPRIS DE GARANTIE DE LA CONCEPTION, DE QUALITÉ MARCHANDE ET<br />"
+                                  " D'ADÉQUATION À UN USAGE PARTICULIER. <br /><br />"); //Insertion du texte
 
-    QVBoxLayout *layout = new QVBoxLayout(); //Affichage du texte et du bouton
-    layout->addWidget(texte);
-    layout->addWidget(closeButton);
+    englishText->setText("<b>About</b><br /><br />"
+                                  "<i>Name</i> : Fluctu'Action<br /><br />"
+                                  "<i>Version</i>  :  1.0<br /><br />"
+                                  "<i>Based on</i> : Qt 5.4.1<br /><br />"
+                                  "Release <br /><br />"
+                                  "<i>Copyright 2015 All right reserved </i><br /><br />"
+                                  "This program is licensed under the terms of the GNU General Public<br /> License version 3<br /><br />"
+                                  "The program is provided AS IS without WARRANTY OF ANY KIND,<br />"
+                                  " INCLUDING WARRANTIES OF DESIGN , MERCHANTABILITY AND<br />"
+                                  " FITNESS FOR A PARTICULAR PURPOSE.<br /><br />"); //Insertion du texte
 
-    setLayout(layout);
 
+
+
+    Tab->addTab(frenchText, "Français");
+    Tab->addTab(englishText, "English");
 }
 
 Help::~Help()
